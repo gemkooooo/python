cfc5a108e22d3f62ddfe7e37111914c9cac67613
gemko <mailbox.gesha@gmail.com>
2024-08-25 20:30:35 +0500

    Correct after check module_1_7_final.py

--- module_1_7_final.py
+++ module_1_7_final.py
@@ -1,17 +1,12 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
-#сортируем по алфавиту
 students = sorted(students)
 #{'Aaron', 'Johnny', 'Bilbo', 'Khendrik', 'Steve'}
 
-averageRatingS0 = sum(list(map(float, grades[0])))/len(list(map(float, grades[0])))
-averageRatingS1 = sum(list(map(float, grades[1])))/len(list(map(float, grades[1])))
-averageRatingS2 = sum(list(map(float, grades[2])))/len(list(map(float, grades[2])))
-averageRatingS3 = sum(list(map(float, grades[3])))/len(list(map(float, grades[3])))
-averageRatingS4 = sum(list(map(float, grades[4])))/len(list(map(float, grades[4])))
-
-averageRating = (averageRatingS0, averageRatingS1, averageRatingS2, averageRatingS3, averageRatingS4)
+averageRating = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]),
+                 sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]),
+                 sum(grades[4])/len(grades[4])]
 
 print(dict(zip(students, averageRating)))
 
